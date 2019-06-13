5913be80dec9be0d7ce4303e8090f1147d246fe5
Jarana Manotumruksa <feay1234@gmail.com>
2019-06-13 18:33:53 +0100

    test senteval on glove and bpe, implemented QQP dataset and pretrained embeddings

--- Dataset.py
+++ Dataset.py
@@ -1,8 +1,12 @@
 from keras.datasets import imdb
 from keras.preprocessing import sequence
 import numpy as np
+from keras_preprocessing.sequence import pad_sequences
 from sklearn.feature_extraction.text import TfidfVectorizer
+from keras.preprocessing.text import Tokenizer
 
+
+import pandas as pd
 
 def get_imbd(max_words=10000, maxlen=500):
     print('Loading data...')
@@ -17,6 +21,71 @@ def get_imbd(max_words=10000, maxlen=500):
     x_test = sequence.pad_sequences(x_test, maxlen=maxlen)
 
     return x_train, y_train, x_test, y_test
+
+
+
+MAX_SEQUENCE_LENGTH = 1000
+MAX_NUM_WORDS = 20000
+EMBEDDING_DIM = 300
+
+def get_datasets(path, names):
+
+    if "QQP":
+
+        df = pd.read_csv(path+"data/glue_data/QQP/train.tsv", sep="\t", error_bad_lines=False )
+        df_val = pd.read_csv(path+"data/glue_data/QQP/dev.tsv", sep="\t", error_bad_lines=False)
+        df_test = pd.read_csv(path+"data/glue_data/QQP/test.tsv", sep="\t", error_bad_lines=False)
+
+        df.question1 = df.question1.astype(str)
+        df.question2 = df.question2.astype(str)
+
+        df_val.question1 = df_val.question1.astype(str)
+        df_val.question2 = df_val.question2.astype(str)
+
+        df_test.question1 = df_test.question1.astype(str)
+        df_test.question2 = df_test.question2.astype(str)
+
+        corpus = df.question1.tolist() + df.question2.tolist() + df_val.question1.tolist() + df_val.question2.tolist() + df_test.question1.tolist() + df_test.question2.tolist()
+
+        # create the tokenizer
+        t = Tokenizer()
+        # fit the tokenizer on the documents
+        t.fit_on_texts(corpus)
+
+        # finally, vectorize the text samples into a 2D integer tensor
+        tokenizer = Tokenizer(num_words=MAX_NUM_WORDS)
+        tokenizer.fit_on_texts(corpus)
+
+        x1_train = tokenizer.texts_to_sequences(df.question1.tolist())
+        x2_train = tokenizer.texts_to_sequences(df.question2.tolist())
+
+        x1_val = tokenizer.texts_to_sequences(df_val.question1.tolist())
+        x2_val = tokenizer.texts_to_sequences(df_val.question2.tolist())
+
+        x1_test = tokenizer.texts_to_sequences(df_test.question1.tolist())
+        x2_test = tokenizer.texts_to_sequences(df_test.question2.tolist())
+
+        x1_train = pad_sequences(x1_train, maxlen=MAX_SEQUENCE_LENGTH)
+        x2_train = pad_sequences(x2_train, maxlen=MAX_SEQUENCE_LENGTH)
+        x_train = [x1_train, x2_train]
+
+        x1_val = pad_sequences(x1_val, maxlen=MAX_SEQUENCE_LENGTH)
+        x2_val = pad_sequences(x2_val, maxlen=MAX_SEQUENCE_LENGTH)
+        x_val = [x1_val, x2_val]
+
+        x1_test = pad_sequences(x1_test, maxlen=MAX_SEQUENCE_LENGTH)
+        x2_test = pad_sequences(x2_test, maxlen=MAX_SEQUENCE_LENGTH)
+        x_test = [x1_test, x2_test]
+
+        y_train = df.is_duplicate.values
+        y_val = df_val.is_duplicate.values
+        y_test = df_test.is_duplicate.values
+
+        word_index = tokenizer.word_index
+        print('Found %s unique tokens.' % len(word_index))
+
+        return x_train, x_test, x_val, y_val, x_test, y_test, word_index
+
 
 
 def get_discriminator_train_data(x_train, x_test, mode="tf"):
--- utils.py
+++ utils.py
@@ -1,2 +1,43 @@
 import numpy as np
+import os
 
+from keras.initializers import Constant
+from keras.layers import Embedding
+
+BASE_DIR = 'data/'
+GLOVE_DIR = os.path.join(BASE_DIR, 'w2v')
+
+def get_pretrain_embeddings(MAX_NUM_WORDS, EMBEDDING_DIM, MAX_SEQUENCE_LENGTH, word_index):
+    print('Indexing word vectors.')
+
+    embeddings_index = {}
+    with open(os.path.join(GLOVE_DIR, 'glove.840B.300d.txt')) as f:
+        for line in f:
+            word, coefs = line.split(maxsplit=1)
+            coefs = np.fromstring(coefs, 'f', sep=' ')
+            embeddings_index[word] = coefs
+
+    print('Found %s word vectors.' % len(embeddings_index))
+
+    print('Preparing embedding matrix.')
+
+    # prepare embedding matrix
+    num_words = min(MAX_NUM_WORDS, len(word_index)) + 1
+    embedding_matrix = np.zeros((num_words, EMBEDDING_DIM))
+    for word, i in word_index.items():
+        if i > MAX_NUM_WORDS:
+            continue
+        embedding_vector = embeddings_index.get(word)
+        if embedding_vector is not None:
+            # words not found in embedding index will be all-zeros.
+            if embedding_vector.shape[0] == 0:
+                continue
+            embedding_matrix[i] = embedding_vector
+
+    # load pre-trained word embeddings into an Embedding layer
+    embedding_layer = Embedding(num_words,
+                                EMBEDDING_DIM,
+                                embeddings_initializer=Constant(embedding_matrix),
+                                input_length=MAX_SEQUENCE_LENGTH)
+
+    return embedding_layer
